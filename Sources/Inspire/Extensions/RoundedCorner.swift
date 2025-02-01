cce8c1ef8761d22eb4189ae80029de34636c17ef
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-01 14:11:19 +0900

    refactor: remove InspireFont and add RoundedCorner shape extension for custom corner radius

---
+++ Sources/Inspire/Extensions/RoundedCorner.swift
@@ -0,0 +1,24 @@
+import SwiftUI 
+
+struct RoundedCorner: Shape {
+    
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+        
+
+    }
+    
+    func radius(_ radius: CGFloat) -> some View {
+        clipShape(RoundedRectangle(cornerRadius: radius))
+    }
+}
